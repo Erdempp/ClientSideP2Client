b2cc65539b06b6c95ee465290308f57d0225ffb0
Erdem <epekguze@avans.nl>
2019-11-22 16:38:32 +0100

    WIP: Routing and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-
-import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { PlaceholderComponent } from './components/placeholder/placeholder.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent
-  ],
+  declarations: [AppComponent, AboutComponent, PlaceholderComponent, NavigationComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot([
-      { path: '', component: AppComponent },
-      { path: 'about', component: AboutComponent },
-    ]),
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatCardModule,
     MatListModule,
     MatTableModule,
+    MatMenuModule,
+    MatButtonModule,
   ],
+  exports: [NavigationComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
